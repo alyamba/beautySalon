e625e5681bbac43efec5bb8faa9aa22df63ee4d0
Alina <logunionok@gmail.com>
2022-11-22 15:10:37 +0300

    feat: add dependencies in pom.xml and add login scene

---
+++ client/src/main/java/com/example/client/Login.java
@@ -0,0 +1,25 @@
+package com.example.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField userPassword;
+
+    @FXML
+    void authorization(ActionEvent event) {
+
+    }
+
+}
