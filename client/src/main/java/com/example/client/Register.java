9b25744dc1c9bbf97cf5ce8ab9fe329c2aa8c150
Alina <logunionok@gmail.com>
2022-11-22 15:33:11 +0300

    feat: add register scene

---
+++ client/src/main/java/com/example/client/Register.java
@@ -0,0 +1,30 @@
+package com.example.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Register {
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private PasswordField userPassword;
+
+    @FXML
+    void registration(ActionEvent event) {
+
+    }
+
+}
