f7196457242618c201b7f4e57541803fb747c360
Alina <logunionok@gmail.com>
2022-11-29 18:43:17 +0300

    feat(add): user control scene

---
+++ client/src/main/java/com/example/client/UserController.java
@@ -0,0 +1,69 @@
+package com.example.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class  {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button masterManagement;
+
+    @FXML
+    private Button serviceManagement;
+
+    @FXML
+    private Button userManagementBtn;
+
+    @FXML
+    private TableView<?> usertable;
+
+    @FXML
+    void addUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void masterControl(ActionEvent event) {
+
+    }
+
+    @FXML
+    void serviceControl(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userControl(ActionEvent event) {
+
+    }
+
+}
