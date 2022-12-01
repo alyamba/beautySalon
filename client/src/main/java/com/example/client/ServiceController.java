170ff3e4fd6d9aeb0a18c6dcafc483812e9bcf2b
Alina <logunionok@gmail.com>
2022-12-01 15:58:27 +0300

    feat(add): scene serviceControl

--- client/src/main/java/com/example/client/ServiceController.java
+++ client/src/main/java/com/example/client/ServiceController.java
@@ -1,4 +1,111 @@
 package com.example.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class ServiceController {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private ImageView createBtn;
+
+    @FXML
+    private TableColumn<?, ?> decriptionService;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private ImageView deleteBtn;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    private ImageView editBtn;
+
+    @FXML
+    private TableColumn<?, ?> idService;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button masterManagement;
+
+    @FXML
+    private TableColumn<?, ?> masterService;
+
+    @FXML
+    private TableView<?> mastertable;
+
+    @FXML
+    private TableColumn<?, ?> nameService;
+
+    @FXML
+    private Button serviceManagement;
+
+    @FXML
+    private TableColumn<?, ?> timeService;
+
+    @FXML
+    private Button userManagementBtn;
+
+    @FXML
+    void addUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createService(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteService(MouseEvent event) {
+
+    }
+
+    @FXML
+    void deleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editService(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void masterControl(ActionEvent event) {
+
+    }
+
+    @FXML
+    void serviceControl(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userControl(ActionEvent event) {
+
+    }
+
 }
