2a3ef088c9fcea957fb9231fd2728cfc2fc7349b
Alina <logunionok@gmail.com>
2022-12-01 15:25:28 +0300

    fix(change): scene userCintrol

--- client/src/main/java/com/example/client/UserController.java
+++ client/src/main/java/com/example/client/UserController.java
@@ -3,24 +3,51 @@ package com.example.client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-public class  {
+public class UserController {
 
     @FXML
     private Button add;
 
     @FXML
+    private ImageView createBtn;
+
+    @FXML
     private Button delete;
 
     @FXML
+    private ImageView deleteBtn;
+
+    @FXML
     private Button edit;
+
+    @FXML
+    private ImageView editBtn;
+
+    @FXML
+    private TableColumn<?, ?> emailUser;
+
+    @FXML
+    private TableColumn<?, ?> firstNameUser;
+
+    @FXML
+    private TableColumn<?, ?> idUser;
+
+    @FXML
+    private TableColumn<?, ?> lastNameUser;
 
     @FXML
     private Button logout;
 
     @FXML
     private Button masterManagement;
+
+    @FXML
+    private TableColumn<?, ?> numberUser;
 
     @FXML
     private Button serviceManagement;
@@ -37,12 +64,17 @@ public class  {
     }
 
     @FXML
-    void deleteUser(ActionEvent event) {
+    void createUser(MouseEvent event) {
 
     }
 
     @FXML
-    void editUser(ActionEvent event) {
+    void deleteUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editUser(MouseEvent event) {
 
     }
 
